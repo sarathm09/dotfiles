09751cc8aa03f767cd6e58116c128f4af62fe95d
Sarath S Menon <sarathm09@gmail.com>
2024-11-26 20:31:29 +0530

    modified macos setup

--- macos/setup.sh
+++ macos/setup.sh
@@ -9,6 +9,34 @@ osascript -e 'tell application "System Preferences" to quit'
 # Ask for the administrator password upfront
 sudo -v
 
+#!/bin/sh
+
+echo "========================================"
+echo "          WELCOME TO THE INSTALL       "
+echo "========================================"
+echo "   This script will set up your system  "
+echo "   by running necessary installation     "
+echo "   scripts in the specified directories. "
+echo "========================================"
+
+# Prompt for user inputs
+
+echo "Enter the following details for setting up the system"
+read -p "Enter the name to set for the system: " computerName
+export computerName
+read -p "Enter the hostname: " hostName
+export hostName
+
+
+echo "Enter the following details for setting up git"
+read -p "Enter your name: " userName
+export userName
+read -p "Enter your email: " userEmail
+export userEmail
+read -p "Enter your GitHub username: " githubUser
+export githubUser
+
+
 # Keep-alive: update existing `sudo` time stamp until `.macos` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
@@ -32,6 +60,11 @@ if [[ -n "$hostName" ]]; then
     sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "$hostName"
 fi
 
+### Install apps using brew
+brew bundle
+
+sudo mkdir -p -m 775 /usr/local/bin
+
 ### Run all setup files
 directories=("apps" "workspace")
 for dir in "${directories[@]}"; do
@@ -50,9 +83,17 @@ for dir in "${directories[@]}"; do
 done
 
 # Create symlinks
-
+# Loop through all .files in the directory
+for script in "config/*"; do
+    if [ -f "$script" ]; then
+        echo "🚀 Linking config $script \n\n"
+        ln -s "$script" "~/$script"
+    fi
+done
 
 # Creating Workspace entries
+chmod +x workspace/setup.sh
+./workspace/setup.sh
 
 # Copying work related config
 
